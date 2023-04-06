02b34c2562863cefc48c1ce4b495add266dfcad9
Fernando Luiz de Lima <fernando.ftec@gmail.com>
2023-04-06 15:41:33 -0300

    The action of configuring keep alive settings is allowed just on windows platform.

--- src/MongoDB.Infrastructure/Internal/ClusterBuilderExtensions.cs
+++ src/MongoDB.Infrastructure/Internal/ClusterBuilderExtensions.cs
@@ -3,6 +3,7 @@ using MongoDB.Driver.Core.Configuration;
 using MongoDB.Driver.Core.Extensions.DiagnosticSources;
 using System;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 
 namespace MongoDB.Infrastructure.Internal
 {
@@ -17,8 +18,17 @@ namespace MongoDB.Infrastructure.Internal
             return clusterBuilder;
         }
 
+        /// <remarks>
+        /// Control sending TCP keep-alive packets and the interval at which they are sent.
+        /// This control code is supported on Windows 2000 and later operating systems.
+        /// </remarks>        
         public static ClusterBuilder ConfigureTcp(this ClusterBuilder clusterBuilder, MongoDbKeepAliveSettings keepAliveSettings)
         {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return clusterBuilder;
+            }
+
             void SocketConfigurator(Socket socket)
             {
                 socket?.IOControl(
